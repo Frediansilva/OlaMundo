5489427352b609e0496d124e9a2a218c8a2d7771
Frediansilva <frediansilva@gmail.com>
2021-05-24 21:46:22 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Tecle para sair");
+            Console.ReadKey();
+
+        }
+    }
+}
